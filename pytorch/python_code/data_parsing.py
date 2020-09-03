fixed message parsing error and added average force vissualization

pytorch/python_code/data_parsing.py
@@ -5,54 +5,49 @@ import json
 import os
 import matplotlib.pyplot as plt
 
-path = 'Data/Failure/4_agents_0_failure_2/Data/'
+episode = '35'
 
-file_names = []
-for file in os.listdir(path):
-    file_names.append(file)
+path = 'Data/Failure/2_agents_0_failure/Data/'
 
-df_list = []
-for i in range(len(file_names)-1):
-    name = 'Data_Episode_'+str(i)+'.csv'
-    file_path = path+name
-    df = pd.read_csv(file_path)
-    df_list.append((i, df))
 
-episode_rewards = []
-losses = []
-epsilons = []
-terminals = []
-for episode in df_list:
-    print(episode[0])
-    rewards = []
-    terminal = []
-    for t in range(len(episode[1])):
-        rewards.append(episode[1]['reward'][t].strip('][').split(','))
-        #epsilons.append(episode[1]['epsilon'][t].strip('][').split(','))
-        #losses.append(episode[1]['loss'][t].strip('][').split(','))
-        #terminal.append(episode[1]['termination'][t])
-    reward = []
-    for robot in range(len(rewards[0])):
-        reward.append(sum(float(row[robot]) for row in rewards))
-    episode_rewards.append(reward)
 
-reward = [row[0] for row in episode_rewards]
-last_10_axis = np.arange(0, len(reward), 10)
-last_10_reward = [sum(reward[i:i + 10])/10
-          for i in last_10_axis[0:len(last_10_axis)-1]]
-if len(last_10_axis) > 150:
-    print('The Best Model is:', 150+np.argmax(last_10_reward[150:]))
+name = 'Data_Episode_'+episode+'.csv'
+file_path = path+name
+df = pd.read_csv(file_path)
+
+rewards = []
+terminal = []
+force_mags = []
+force_angs = []
+average_force_vec = []
+for t in range(len(df)):
+    rewards.append(df['reward'][t].strip('][').split(','))
+    #epsilons.append(df['epsilon'][t].strip('][').split(','))
+    #losses.append(df['loss'][t].strip('][').split(','))
+    #terminal.append(df['termination'][t])
+    force_mags.append(df['force magnitude'][t].strip('][').split(','))
+    force_angs.append(df['force angle'][t].strip('][').split(','))
+    average_force_vec.append(df['average force vector'][t].strip('][').split(','))
+robot_forces = []
+for i in range(len(force_mags[0])):
+    forces = []
+    for j in range(len(force_mags)):
+        forces.append(float(force_mags[j][i]))
+    robot_forces.append(forces)
+mag = []
+ang = []
+for t in range(len(average_force_vec)):
+    mag.append(float(average_force_vec[t][0]))
+    ang.append(average_force_vec[t][1])
+axis = np.arange(0, len(mag))
 
 plt.figure(num=None, figsize=(20, 12), dpi=80, facecolor='w', edgecolor='k')
-plt.title('4 Agent Double Deep Q-Learning\n with 0 Failure Leanring')
-plt.xlabel('Episodes')
-plt.ylabel('Reward')
-s = ['1.9', '1.8', '1.7', '1.6', '1.5', '1.4', '1.3', '1.2', '1.1', '1.0', '0.9', '0.8', '0.7', '0.6', '0.5' ]
-x = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
-#[250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750]
-y = [-9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200]
-for i in range(0, min(math.floor(len(reward)/100), len(s))):
-    plt.text(x[i], y[i], s[i], c='gray')
-plt.plot(reward, c = 'lightsteelblue')
-plt.plot(last_10_axis[1:len(last_10_axis)], last_10_reward, c = 'b')
-plt.savefig('Data/Figures/4_agents_0_failure_2.png')
+for i in range(len(robot_forces)):
+    l = 'robot '+str(i+1) + 'force'
+    plt.plot(robot_forces[i], label = l)
+plt.plot(axis, mag, c='k', label = 'Cumulative Force')
+plt.xlabel('Time')
+plt.title('Force over Time for Episode '+episode)
+plt.ylabel('Force')
+plt.legend()
+plt.savefig('Data/Figures/2_agents_0_failure_Force_episode_'+episode+'.png')

pytorch/python_code/viz.py
@@ -1,61 +1,58 @@
-import json
-import os
 import pandas as pd
 import numpy as np
+import math
+import json
+import os
 import matplotlib.pyplot as plt
 
-path_to_json = 'Data/test1'
-path_to_Model_Info_json = 'Data/test/Model_Info/'
-path_to_Loss_Info_json = 'Data/test/Loss_Info/'
+path = 'Data/Failure/2_agents_0_failure/Data/'
 
-# Load all robot data
-json_Model_file_names = [pos_json for pos_json in os.listdir(path_to_Model_Info_json) if pos_json.endswith('.json')]
-json_Loss_file_names = [pos_json for pos_json in os.listdir(path_to_Loss_Info_json) if pos_json.endswith('.json')]
-fig, (ax1, ax2, ax3) = plt.subplots(3)
-while True:
-    modelData = []
-    lossData = []
-    for file in json_Model_file_names:
-        with open(path_to_Model_Info_json+file) as f:
-            modelData.append(json.load(f))
-    for file in json_Loss_file_names:
-        with open(path_to_Loss_Info_json+file) as f:
-            lossData.append(json.load(f))
-    epsilons = []
+file_names = []
+for file in os.listdir(path):
+    file_names.append(file)
+
+df_list = []
+for i in range(len(file_names)-1):
+    name = 'Data_Episode_'+str(i)+'.csv'
+    file_path = path+name
+    df = pd.read_csv(file_path)
+    df_list.append((i, df))
+
+episode_rewards = []
+losses = []
+epsilons = []
+terminals = []
+for episode in df_list:
+    print(episode[0])
     rewards = []
-    losses = []
-    for robot in range(len(modelData)):
-        epsilon = []
-        reward = []
-        old_j = 0
-        for episode_num in modelData[robot]:
-            epsilon.append(modelData[robot][episode_num][0])
-            reward.append(modelData[robot][episode_num][1])
-        epsilons.append(epsilon)
-        rewards.append(reward)
-    for robot in range(len(lossData)):
-        loss = []
-        for episode_num in range(len(lossData[robot])):
-            loss.append(lossData[robot][episode_num])
-        losses.append(loss)
+    terminal = []
+    for t in range(len(episode[1])):
+        rewards.append(episode[1]['reward'][t].strip('][').split(','))
+        #epsilons.append(episode[1]['epsilon'][t].strip('][').split(','))
+        #losses.append(episode[1]['loss'][t].strip('][').split(','))
+        #terminal.append(episode[1]['termination'][t])
+    reward = []
+    for robot in range(len(rewards[0])):
+        reward.append(sum(float(row[robot]) for row in rewards))
+    episode_rewards.append(reward)
 
-    fig.suptitle('Single Agent')
-    ax1.set_ylabel('Epsilon')
-    ax2.set_ylabel('Reward')
-    ax3.set_ylabel('Loss')
-    for robot in range(len(modelData)):
-        ax1.plot(epsilons[robot], label = 'model '+str(robot+1))
-        ax2.plot(rewards[robot], label = 'model '+str(robot+1))
-    for robot in range(len(lossData)):
-        ax3.plot(losses[robot], label = 'model '+str(robot+1))
-    ax1.axes.get_xaxis().set_visible(False)
-    #plt.xticks(np.arange(0, len(epsilons[0])+1, 50.0))
+reward = [row[0] for row in episode_rewards]
+last_10_axis = np.arange(0, len(reward), 10)
+last_10_reward = [sum(reward[i:i + 10])/10
+          for i in last_10_axis[0:len(last_10_axis)-1]]
+if len(last_10_axis) > 150:
+    print('The Best Model is:', 150+np.argmax(last_10_reward[150:]))
 
-    ax1.legend()
-    plt.xlabel('Episodes/ Time Steps')
-    plt.pause(15)
-    print('Updating plot')
-    plt.savefig(path_to_json+'charts.png')
-    ax1.cla()
-    ax2.cla()
-    ax3.cla()
+plt.figure(num=None, figsize=(20, 12), dpi=80, facecolor='w', edgecolor='k')
+plt.title('2 Agent Double Deep Q-Learning\n with 0 Failure Leanring')
+plt.xlabel('Episodes')
+plt.ylabel('Reward')
+s = ['1.9', '1.8', '1.7', '1.6', '1.5', '1.4', '1.3', '1.2', '1.1', '1.0', '0.9', '0.8', '0.7', '0.6', '0.5' ]
+x = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
+#[250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750]
+y = [-9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200, -9200]
+for i in range(0, min(math.floor(len(reward)/100), len(s))):
+    plt.text(x[i], y[i], s[i], c='gray')
+plt.plot(reward, c = 'lightsteelblue')
+plt.plot(last_10_axis[1:len(last_10_axis)], last_10_reward, c = 'b')
+plt.savefig('Data/Figures/2_agents_0_failure.png')