b261ba1682cd4084ce982bd574070584a6368f16
Josh Bloom <jdbloom@wpi.edu>
2021-03-08 11:30:46 -0500

    working with new argos

--- pytorch/Main.py
+++ pytorch/Main.py
@@ -88,7 +88,7 @@ while not exp_done:
     data_file_name = 'Data_Episode_'+str(ep_counter)+'.csv'
     with open(data_file_path+data_file_name, 'w') as output:
         writer = csv.writer(output, delimiter = ',')
-        writer.writerow(['reward', 'epsilon', 'termination', 'force magnitude', 'force angle', 'average force vector'])
+        writer.writerow(['reward', 'epsilon', 'termination', 'messages', 'force magnitude', 'force angle', 'average force vector'])
 
         if not exp_done:
             time_steps = 0
@@ -203,7 +203,7 @@ while not exp_done:
                     # Store New Observations
                     agent_states = new_agent_states
                     actions = []
-                    message_codes = []
+
 
                     # Calculate average force vector
                     average_force_mag = None
@@ -217,8 +217,8 @@ while not exp_done:
                             average_force_mag = math.sqrt(average_force_mag**2 + force_mags[i]**2 + 2*(average_force_mag)*(force_mags[i])*math.cos(math.radians(angle)))
                             average_force_ang = math.asin(force_mags[i]*math.sin(math.radians(180 - angle)) / average_force_mag)
 
-                    writer.writerow([r, model.epsilon, reached_goal, force_mags, force_angs, [average_force_mag, math.degrees(average_force_ang)]])
-
+                    writer.writerow([r, model.epsilon, reached_goal, message_codes, force_mags, force_angs, [average_force_mag, math.degrees(average_force_ang)]])
+                    message_codes = []
                     if episode_done:
                         run_time = time.time() - exp_start_time
                         print('[RUN TIME] %.2f' % run_time)
---
+++ pytorch/python_code/comms_viz.py
@@ -0,0 +1,50 @@
+import argparse
+import pandas as pd
+import numpy as np
+import math
+import json
+import os
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("data_path")
+parser.add_argument("figure_path")
+parser.add_argument("figure_name")
+parser.add_argument("Episode")
+args = parser.parse_args()
+
+data_path = args.data_path + 'Data/'
+
+
+name = 'Data_Episode_'+args.Episode+'.csv'
+file_path = data_path+name
+df = pd.read_csv(file_path)
+messages = []
+for t in range(len(df)):
+    messages.append(df['messages'][t].strip('][').split(','))
+
+fig, axs = plt.subplots(2, 2)
+ax = [axs[0][0], axs[0][1], axs[1][0], axs[1][1]]
+
+time = np.arange(0, len(messages), 1)
+for r in range(len(messages[0])):
+    robot_messages = []
+    msg_1 = 0
+    msg_2 = 0
+    msg_3 = 0
+    msg_4 = 0
+    for i in range(len(messages)):
+        msg = int(messages[i][r])
+        robot_messages.append(msg)
+        if msg == 0: msg_1 += 1
+        if msg == 1: msg_2 += 1
+        if msg == 2: msg_3 += 1
+        if msg == 3: msg_4 += 1
+    ax[r].plot(time, robot_messages, label = 'robot '+str(r))
+    ax[r].legend(loc=1)
+    print('------ ROBOT:', r, '--------')
+    print('MSG 1 = %0.2f' % (msg_1/len(messages)))
+    print('MSG 2 = %0.2f' % (msg_2/len(messages)))
+    print('MSG 3 = %0.2f' % (msg_3/len(messages)))
+    print('MSG 4 = %0.2f' % (msg_4/len(messages)))
+plt.show()
