59517fd2f8c009898ab751d44dd1979138e28441
Josh Bloom <jdbloom@wpi.edu>
2021-05-24 13:01:59 -0400

    pushing working code for carlo

--- argos/collectiveRlTransport.cpp
+++ argos/collectiveRlTransport.cpp
@@ -294,7 +294,8 @@ void CCollectiveRLTransport::CreateEntities() {
         );
      for(size_t i = 0; i < m_unNumEpisodes; ++i){
        if(update_offset_flag == 1){
-         if((i+1)%m_unGateUpdateFrequency == 0){
+         /** delay gate obstacle for 150 episodes to allow algorithm to move to goal first*/
+         if((i)%m_unGateUpdateFrequency == 0){
            offset = offset - m_fGateUpdate;
            std::cout<<"Updating gap distance to "<<offset*2<<" at episode "<<i<<std::endl;
            if(offset <= (m_fGateMinimum/2.0)){
