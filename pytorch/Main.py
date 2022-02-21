fixed proximity sensors average

pytorch/Main.py
@@ -385,7 +385,7 @@ while not exp_done:
                             prox_values = env_observations[i][7:]
                             #print('[DEBUG] Prox Values', prox_values)
                             prox_value = np.sum(prox_values)                ### Change to actual values instead of binary flag
-                            #agent_prox_flags.append(prox_value)
+                            #agent_prox_flags.append(prox_value/24.0)
                             if prox_value/24 > 0.5:
                                 agent_prox_flags.append(1)
                             else: