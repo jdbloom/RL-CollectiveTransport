83272b62f86b3174556c73377a5d31cfea8dd71f
Joshua Bloom <jdbloom@wpi.edu>
2022-06-17 14:22:38 -0400

    added observations and actions to csv for MME

--- pytorch/Main.py
+++ pytorch/Main.py
@@ -126,7 +126,7 @@ while not exp_done:
     data_file_name = 'Data_Episode_'+str(ep_counter)+'.csv'
     with open(data_file_path+data_file_name, 'w') as output:
         writer = csv.writer(output, delimiter = ',')
-        writer.writerow(['reward', 'epsilon', 'termination', 'loss', 'force magnitude', 'force angle', 'average force vector', 'cyl_x_pos', 'cyl_y_pos', 'gate_stats', 'obstacle_stats', 'intention_reward', 'run_time', 'robots_x_pos', 'robots_y_pos', 'robot_angle'])
+        writer.writerow(['reward', 'epsilon', 'termination', 'loss', 'force magnitude', 'force angle', 'average force vector', 'cyl_x_pos', 'cyl_y_pos', 'gate_stats', 'obstacle_stats', 'intention_reward', 'run_time', 'robots_x_pos', 'robots_y_pos', 'robot_angle', 'env_observations', 'agent_actions'])
 
         if not exp_done:
             time_steps = 0
@@ -418,7 +418,7 @@ while not exp_done:
                     writer.writerow([r, tmp_epsilon, reached_goal, loss, force_mags, force_angs, 
                                     [average_force_mag, math.degrees(average_force_ang)], obj_stats[0], obj_stats[1],
                                     gate, obstacles, intention_reward, time.time() - episode_start_time, robot_x_pos, 
-                                    robot_y_pos, robot_angle])
+                                    robot_y_pos, robot_angle, env_observations, actions_to_take])
 
                     if episode_done:
                         run_time = time.time() - episode_start_time
