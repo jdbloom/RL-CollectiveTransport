a3a03c8bacb272874e1b26a38f7e9cae45145839
Chandler Garcia <crgarcia@wpi.edu>
2024-09-10 10:44:23 -0400

    need to do some cleaning

--- pytorch/python_code/cmRewardsDisPlot.py
+++ pytorch/python_code/cmRewardsDisPlot.py
@@ -15,12 +15,17 @@ parser = argparse.ArgumentParser()
 # parser.add_argument("figure_path")
 # parser.add_argument("figure_name")
 parser.add_argument("--source_path")
-parser.add_argument("--Episode", default=0)
+parser.add_argument("--episodes", default=999)
 parser.add_argument("--num_robots", default = 4)
+parser.add_argument("--intention", default=False, action="store_true")
+parser.add_argument("--object_radius", default = 0.12)
 
 args = parser.parse_args()
 
 data_path = args.source_path
+
+n_eps = int(args.episodes)
+object_radius = float(args.object_radius)
 
 print('. . . Loading Model Data')
 file_names = []
@@ -28,8 +33,8 @@ for file in os.listdir(data_path):
     file_names.append(file)
 
 df_list = []
-for i in range(len(file_names)-1):
-    name = 'Data_Episode_'+str(i)+'.csv'
+for i in range(n_eps):
+    name = '/Data_Episode_'+str(i)+'.csv'
     file_path = data_path+name
     df = pd.read_csv(file_path)
     df_list.append((i, df))
@@ -38,110 +43,122 @@ episode_rewards = []
 episode_intentions = []
 episode_intention_rewards = []
 
+rewards = []
+rewards_std = []
+intention_reward = []
+intention_std = []
+prediction_offset = []
+prediction_offset_std = []
+i_prediction_offset = []
+i_prediction_offset_std = []
+
+n_robots = int(args.num_robots)
+
 print('. . . Consolodating Model Data')
 IL_flag = False
+
 for episode in df_list:
-    rewards = []
-    intention_reward = []
-    for t in range(len(episode[1])):
-        rewards.append(episode[1]['reward'][t].strip('][').split(','))
-        intention_reward.append(episode[1]['intention_reward'][t].strip('][').split(','))
+    pos_mx_cyl = np.asarray(episode[1]['cyl_mod_x'])
+    pos_my_cyl = np.asarray(episode[1]['cyl_mod_y'])
+    cyl_radius = np.asarray(episode[1]['cyl_radius'])
+    pred_x_robots = np.empty([len(episode[1]['agent_predictions_x']),n_robots])
+    pred_y_robots = np.empty([len(episode[1]['agent_predictions_y']),n_robots])
+    pos_x_robots = np.empty([len(episode[1]['robots_x_pos']),n_robots])
+    pos_y_robots = np.empty([len(episode[1]['robots_y_pos']),n_robots])
+    intention_pred_x = np.empty([len(episode[1]['agent_predictions_y']),n_robots])
+    intention_pred_y = np.empty([len(episode[1]['agent_predictions_y']),n_robots])
+    intention_error_cm = np.empty([len(episode[1]['agent_predictions_y'])])
+    r = np.empty([len(episode[1]['agent_predictions_y']),n_robots])
+    inte = np.empty([len(episode[1]['agent_predictions_y']),n_robots])
+    for i in range(len(pos_mx_cyl)): # For each episode
+        r[i] = np.fromstring(episode[1]['reward'][i].strip(']['), sep=',')
+        inte[i] = np.fromstring(episode[1]['intention_reward'][i].strip(']['), sep=',')
 
-    robot_rewards = []
-    robot_intentions = []
-    for i in range(len(rewards[0])):
-        tmp_r = 0
-        tmp_i = 0
-        for j in range(len(rewards)):
-                if rewards[j][0] != 'reward':
-                    tmp_r += float(rewards[j][i])
-                    tmp_i += float(intention_reward[j][i])
+        if args.intention:
+            # intention_angle = np.fromstring(episode[1]['intention_angle'][i].strip(']['), sep=' ')
+            intention_radius = np.fromstring(episode[1]['intention_radius'][i].strip(']['), sep=' ')[0]
+            intention_error_cm = np.fromstring(episode[1]['intention_error'][i].strip(']['), sep=' ')
+
+            pos_x_robots[i] = np.fromstring(episode[1]['robots_x_pos'][i].strip('[]'), sep=',')
+            pos_y_robots[i] = np.fromstring(episode[1]['robots_y_pos'][i].strip('[]'), sep=',')
+
+            # rob_to_cyl_ang = np.arctan2(pos_my_cyl[i] - pos_y_robots[i], pos_mx_cyl[0] - pos_x_robots[i])
+            # angle_modified = rob_to_cyl_ang + intention_angle * math.pi / 2
+            # intention_pred_x[i] = np.cos(angle_modified) * intention_radius * cyl_radius[1] + pos_x_robots[i]
+            # intention_pred_y[i] = np.sin(angle_modified) * intention_radius * cyl_radius[1] + pos_y_robots[i]
 
 
-        robot_rewards.append(tmp_r)
-        robot_intentions.append(tmp_i)
-    episode_rewards.append(robot_rewards)
-    episode_intentions.append(robot_intentions)
+        pred_x_robots[i] = np.fromstring(episode[1]['agent_predictions_x'][i].strip('[]'), sep=',')
+        pred_y_robots[i] = np.fromstring(episode[1]['agent_predictions_y'][i].strip('[]'), sep=',')
     
-robot_exp_rewards = []
-robot_exp_rewards_avg = []
-robot_exp_intentions = []
-robot_exp_intentions_avg = []
-for j in range(len(episode_rewards[0])):
-    robot_exp_rewards.append([])
-    robot_exp_rewards_avg.append([])
-    robot_exp_intentions.append([])
-    robot_exp_intentions_avg.append([])
-    episode_success_reward.append([])
-    episode_success_index.append([])
-    episode_failure_reward.append([])
-    episode_failure_index.append([])
-    for i in range(len(episode_rewards)):
-        robot_exp_rewards[j].append(episode_rewards[i][j])
-        robot_exp_rewards_avg[j].append(episode_rewards[i][j]/episode_time_steps[i])
-        robot_exp_intentions[j].append(episode_intentions[i][j])
-        robot_exp_intentions_avg[j].append(episode_intentions[i][j]/episode_time_steps[i])
+    error = np.sqrt(np.square(np.subtract(pred_x_robots,pos_mx_cyl[:,None])) + np.square(np.subtract(pred_y_robots,pos_my_cyl[:,None])))
+    if args.intention:
+        # ierror = np.sqrt(np.square(np.subtract(intention_pred_x,pos_mx_cyl[:,None])) + np.square(np.subtract(intention_pred_y,pos_my_cyl[:,None])))
+        ierror = intention_error_cm
+    else:
+        ierror = 0
 
-print('\n. . . Live Plotting Full Experiment '+args.Episode+' . . .')
+    rewards.append(np.mean(r))
+    rewards_std.append(np.std(r))
+    intention_reward.append(np.mean(inte))
+    intention_std.append(np.std(inte))
 
-CB_color_cycle = ['#377eb8', '8e41e0', 'e04161', 'cf6730','c91818', '#ff7f00', '#4daf4a',
-    '#f781bf', '#a65628', '#984ea3',
-    '#999999', '#dede00']
+    prediction_offset.append(np.mean(error))
+    prediction_offset_std.append(np.std(error))
+
+    i_prediction_offset.append(np.mean(ierror))
+    i_prediction_offset_std.append(np.std(ierror))
+
+rewards = np.asarray(rewards)
+rewards_std = np.asarray(rewards_std)
+intention_reward = np.asarray(intention_reward)
+print(intention_reward)
+intention_std = np.asarray(intention_std)
+print(intention_std)
+prediction_offset = np.asarray(prediction_offset)
+prediction_offset_std = np.asarray(prediction_offset_std) 
+i_prediction_offset = np.asarray(i_prediction_offset)
+i_prediction_offset_std = np.asarray(i_prediction_offset_std) 
+
+print('\n. . . Live Plotting Full Experiment From Episodes 0 to '+ str(args.episodes) +' . . .')
+
+success_colors = ['darkturquoise', 'lightgreen', 'khaki', 'violet', 'lightcoral', 'sandybrown', 'mediumpurple', 'plum']
+fail_colors = ['steelblue', 'yellowgreen', 'darkkhaki', 'darkviolet', 'tomato', 'chocolate', 'crimson', 'orangered']
+avg_colors = ['blue', 'green', 'gold', 'purple', 'red', 'saddlebrown', 'yellowgreen', 'darkcyan']
 
 plt.figure(num=None, figsize=(20, 12), dpi=80, facecolor='w', edgecolor='k')
 plt.rcParams.update({'font.size': 22})
 plt.xlabel('Episodes')
 plt.ylabel('Reward')
-for i in range(len(episode_success_reward)):
+plt.title("Training Rewards")
 
-fig = plt.figure(figsize=(20,10))
-plt.rcParams.update({'font.size': 22})
+plt.plot(np.linspace(0,n_eps,n_eps), rewards, c = success_colors[0], label = "Rewards")
+plt.fill_between(np.linspace(0,n_eps,n_eps), rewards - rewards_std, rewards + rewards_std, color=success_colors[0], alpha = 0.25)
+if args.intention:
+    plt.plot(np.linspace(0,n_eps,n_eps), intention_reward, c = success_colors[1], label = "Intention Assisted Rewards")
+    plt.fill_between(np.linspace(0,n_eps,n_eps), intention_reward - intention_std, intention_reward + intention_std, color=success_colors[1], alpha = 0.25)
 
-opaqueness = 5
-robot_center_op = 0.5
-prediction_op = 0.8
-decrement = 0.05
-
-cyl = plt.plot([],[], c=CB_color_cycle[0], linewidth=3)
-pos_r = [[] for i in range(args.num_robots)]
-pos_p = [[] for i in range(args.num_robots)]
-for i in range(args.num_robots):
-    for j in range(opaqueness):
-        pos_r[i].append(plt.plot([],[], c=CB_color_cycle[(1 + 2*i)%len(CB_color_cycle)], alpha=robot_center_op - j*decrement)[0])
-        pos_p[i].append(plt.plot([],[], c=CB_color_cycle[(2 + 2*i)%len(CB_color_cycle)], alpha=prediction_op - j*decrement)[0])
-
-
-plt.xlabel('X Position')
-plt.ylabel('Y Position')
-plt.title('Robot Positions and Predictions in relation to Center of Mass')
-
-# metadata = dict(title='Center of Mass', artist='Chandler Garcia')
-# writer = animation.PillowWriter(fps=15, metadata=metadata)
-
-# with writer.saving(fig, "CenterOfMassPrediction.gif", len(pos_x_cyl)):
-#     for i in range(len(pos_x_cyl)):
-
-visible_interval = 600
-opacity_change = int(visible_interval / opaqueness)
-
-def animate(i):
-    cyl[0].set_data(pos_x_cyl[max(i-visible_interval, 0):i],pos_y_cyl[max(i-visible_interval, 0):i])
-    for j in range(args.num_robots):
-        for k in range(opaqueness):
-            start = max(i - (opacity_change * k), 0)
-            end = max(i - (opacity_change * (1 + k)), 0)
-            pos_r[j][k].set_data(pos_x_robots[end:start,j],pos_y_robots[end:start,j])
-            pos_p[j][k].set_data(pred_x_robots[end:start,j],pred_y_robots[end:start,j])
-    # print(list(chain.from_iterable(pos_r)))
-    return cyl[0], *list(chain.from_iterable(pos_r)), *list(chain.from_iterable(pos_p))
-            
-ani = animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50, frames = len(pos_x_robots))
-
-plt.axis([-1,1,-0.25,1.75])
-
-writer = animation.PillowWriter(fps=15, metadata=dict(artist='Chandler Garcia'), bitrate=1800)
+plt.legend(loc='upper right')
 
 plt.show()
 
-plt.savefig(CM_Plot_+'_'+str(episodes[j])+'.png')
+plt.close()
+
+plt.figure(num=None, figsize=(20, 12), dpi=80, facecolor='w', edgecolor='k')
+plt.rcParams.update({'font.size': 22})
+plt.xlabel('Episodes')
+plt.ylabel('Prediction Accuracy (cm)')
+plt.title('Robot Predictions Error')
+plt.plot(np.linspace(0,n_eps, n_eps), prediction_offset, c=avg_colors[1], label = "Unassisted Prediction")
+plt.fill_between(np.linspace(0,n_eps,n_eps), prediction_offset - prediction_offset_std, prediction_offset + prediction_offset_std, color=avg_colors[1], alpha = 0.25)
+
+if args.intention:
+    print("Also showing intention on graph")
+    plt.plot(np.linspace(0,n_eps, n_eps), i_prediction_offset, c=avg_colors[3], label = "Intention Assisted Prediction")
+    plt.fill_between(np.linspace(0,n_eps,n_eps), i_prediction_offset - i_prediction_offset_std, i_prediction_offset + i_prediction_offset_std, color=avg_colors[3], alpha = 0.25)
+
+plt.legend(loc='upper right')
+
+plt.show()
+
 plt.close()
